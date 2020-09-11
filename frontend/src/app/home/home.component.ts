9695fa46558c553e78c736c367fc3b0c70c712e3
Kanchan <kanchankandage@gmail.com>
2020-09-11 10:56:07 -0400

    Added HttpHeaders

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,22 +1,27 @@
-import { CatDetailsService } from './../cat-details.service';
-import { Component, OnInit } from '@angular/core';
+import { CatDetailsService } from "./../cat-details.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private catServ: CatDetailsService) {}
-  cats: any;
-  ngOnInit(): void {
-    this.getData();
-  }
+	constructor(private catServ: CatDetailsService) {}
+	cats: any;
+	ngOnInit(): void {
+		this.getData();
+	}
 
-  getData = () => {
-    this.catServ.getData().subscribe((response) => {
-      console.log('Data stuff yo', response);
-      // this.cats = response.data.children;
-    });
-  };
+	getData = () => {
+		this.catServ.getData().subscribe(
+			(response) => {
+				console.log("Data stuff yo", response);
+				// this.cats = response.data.children;
+			},
+			(error) => {
+				console.log(error.message);
+			}
+		);
+	};
 }
